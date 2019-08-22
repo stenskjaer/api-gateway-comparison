ba37b15f0734f995a14cab081a1776efadc503b6
Michael Stenskjær Christensen <mschristensen@computas.com>
2019-08-22 16:32:14 +0200

    Disable authentication with cloud endpoints

--- 2-cloud-datastore/app/controllers/base_api_controller.rb
+++ 2-cloud-datastore/app/controllers/base_api_controller.rb
@@ -1,17 +1,17 @@
 class BaseApiController < ApplicationController
-    before_filter :authenticate_user_from_token!
+    # before_filter :authenticate_user_from_token!
 
-    private
-        def authenticate_user_from_token!
-            valid_keys = ["veryS3cr4tT0kun", "f4variteS3cre7"]
-            token = request.headers["X-Api-Key"]
-            if !request.headers["X-Api-Key"]
-                render json: {status: "unauthorized", message: "No API key given in header."}, status: :unauthorized
-            else
-                if !valid_keys.include?(request.headers["X-Api-Key"])
-                    render json: {status: "unauthorized", message: "Invalid API key."}, status: :unauthorized
-                end
-            end
-        end
+    # private
+    #     def authenticate_user_from_token!
+    #         valid_keys = ["veryS3cr4tT0kun", "f4variteS3cre7"]
+    #         token = request.headers["X-Api-Key"]
+    #         if !request.headers["X-Api-Key"]
+    #             render json: {status: "unauthorized", message: "No API key given in header."}, status: :unauthorized
+    #         else
+    #             if !valid_keys.include?(request.headers["X-Api-Key"])
+    #                 render json: {status: "unauthorized", message: "Invalid API key."}, status: :unauthorized
+    #             end
+    #         end
+    #     end
 
 end
