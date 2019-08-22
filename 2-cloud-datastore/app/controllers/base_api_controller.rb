ba6ce2e1b668eda906bda4b3fed73b366763123c
Michael Stenskjær Christensen <mschristensen@computas.com>
2019-08-22 10:53:37 +0200

    Refactor the secret logic to be slightly less stupid

--- 2-cloud-datastore/app/controllers/base_api_controller.rb
+++ 2-cloud-datastore/app/controllers/base_api_controller.rb
@@ -3,11 +3,12 @@ class BaseApiController < ApplicationController
 
     private
         def authenticate_user_from_token!
+            valid_keys = ["veryS3cr4tT0kun", "f4variteS3cre7"]
             token = request.headers["X-Api-Key"]
             if !request.headers["X-Api-Key"]
                 render json: {status: "unauthorized", message: "No API key given in header."}, status: :unauthorized
             else
-                if !["veryS3cr4tT0kun"].include?(request.headers["X-Api-Key"])
+                if !valid_keys.include?(request.headers["X-Api-Key"])
                     render json: {status: "unauthorized", message: "Invalid API key."}, status: :unauthorized
                 end
             end
