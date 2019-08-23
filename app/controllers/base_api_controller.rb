52a2dfe43a94a31b85189617d7a27d08b76e145a
Michael Stenskjær Christensen <michael.stenskjaer@gmail.com>
2019-08-23 13:10:44 +0200

    Use the naive API key authentication approach

--- app/controllers/base_api_controller.rb
+++ app/controllers/base_api_controller.rb
@@ -1,3 +1,16 @@
 class BaseApiController < ApplicationController
+    before_filter :authenticate_user_from_token!
 
+    private
+        def authenticate_user_from_token!
+            valid_keys = ["veryS3cr4tT0kun", "f4variteS3cre7"]
+            token = request.headers["X-Api-Key"]
+            if !request.headers["X-Api-Key"]
+                render json: {status: "unauthorized", message: "No API key given in header."}, status: :unauthorized
+            else
+                if !valid_keys.include?(request.headers["X-Api-Key"])
+                    render json: {status: "unauthorized", message: "Invalid API key."}, status: :unauthorized
+                end
+            end
+        end
 end
